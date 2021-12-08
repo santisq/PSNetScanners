1cd1e29c41fcb8123847c81576af611b8e3c3230
Santiago Squarzon <squarzon.santiago@gmail.com>
2021-12-08 12:47:44 -0300

    Create Network-IPScanner.ps1

---
+++ Network-IPScanner.ps1
@@ -0,0 +1,132 @@
+# Define List of IPs / Hosts to Ping
+# $list = 'host1', 'host2', 'host3'
+$list = 1..254 | ForEach-Object {
+    "192.168.1.$_"
+}
+
+function Pinger {
+[cmdletbinding(DefaultParameterSetName = 'Count')]
+param(
+    [parameter(
+        Mandatory,
+        ValueFromPipeline,
+        Position = 0
+    )]
+    [string]$Address,
+    [parameter(
+        ParameterSetName = 'Count',
+        Position = 1
+    )]
+    [int]$Count = 1,
+    [parameter(
+        Position = 2
+    )]
+    [int]$TimeOut = 1000,
+    [parameter(
+        ParameterSetName = 'Quiet',
+        Position = 1
+    )]
+    [switch]$Quiet,
+    [parameter(
+        ParameterSetName = 'Buffer',
+        Position = 3
+    )]
+    [string]$Buffer = 'aaaaaaaaaa'
+)
+
+    process
+    {
+        $ping = [System.Net.NetworkInformation.Ping]::new()
+        $options = [System.Net.NetworkInformation.PingOptions]::new()
+        $options.DontFragment = $true
+        $data = [System.Text.Encoding]::Unicode.GetBytes($Buffer)
+        $hostname = [System.Net.Dns]::GetHostName()
+
+        if($Quiet)
+        {
+            return [bool]$ping.Send(
+                $Address,
+                $TimeOut,
+                $data,
+                $options
+            ).RoundtripTime
+        }
+
+        1..$Count | ForEach-Object {
+
+            $response = $ping.Send($Address, $TimeOut, $data, $options)
+            
+            $latency = (
+                '*',
+                [string]::Format(
+                    '{0} ms',
+                    $response.RoundtripTime
+                )
+            )[$response.Status -eq 'Success']
+
+            $resolver = try
+            {
+                [System.Net.Dns]::GetHostEntry($Address).HostName
+            }
+            catch
+            {
+                '*'
+            }
+
+            [pscustomobject]@{
+                Ping     = $_
+                Source   = $hostname
+                Address  = $Address
+                HostName = $resolver
+                Latency  = $latency
+                Status   = $response.Status
+            }
+        }
+    }
+}
+
+# Store the function as String
+$funcDef = "function Pinger { $function:Pinger }"
+
+# Change this value for tweaking
+$Threshold = 100
+$RunspacePool = [runspacefactory]::CreateRunspacePool(1, $Threshold)
+$RunspacePool.Open()
+
+$scriptBlock = {
+    param([string]$ip, [string]$Pinger)
+
+    # Load the function in this Scope
+    . ([scriptblock]::Create($Pinger))
+
+    Pinger -Address $ip
+}
+
+$runspaces = foreach($ip in $list)
+{
+    $params = @{
+        IP = $ip
+        Pinger = $funcDef
+    }
+
+    $psinstance = [powershell]::Create().AddScript($scriptBlock).AddParameters($params)
+    $psinstance.RunspacePool = $RunspacePool
+    
+    [pscustomobject]@{
+        Instance = $psinstance
+        Handle = $psinstance.BeginInvoke()
+    }
+}
+
+while($runspaces.Handle.IsCompleted -contains $false)
+{
+    Start-Sleep -Milliseconds 500
+}
+
+$results = $runspaces.ForEach({
+    $_.Instance.EndInvoke($_.Handle)
+    $_.Instance.Dispose()
+})
+
+$runspaces.Clear()
+$RunspacePool.Dispose()
