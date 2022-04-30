87005938ac73534a4f957b970f4dcaf154237a90
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-04-30 11:52:46 -0300

    refactoring

--- Test-TCPConnectionAsync.ps1
+++ Test-TCPConnectionAsync.ps1
@@ -2,7 +2,7 @@ using namespace System.Diagnostics
 using namespace System.Collections.Generic
 using namespace System.Collections.Specialized
 using namespace System.Net.Sockets
-using namespace System.Threading
+using namespace System.Threading.Tasks
 
 function Test-TCPConnectionAsync {
     [cmdletbinding()]
@@ -23,37 +23,10 @@ function Test-TCPConnectionAsync {
     begin {
         $timer = [Stopwatch]::StartNew()
         $tasks = [List[OrderedDictionary]]::new()
-
-        function Wait-Tasks {
-            do {
-                $id = [WaitHandle]::WaitAny($tasks.Task.AsyncWaitHandle, 200)
-                if($id -eq [WaitHandle]::WaitTimeout) {
-                    continue
-                }
-                $instance, $task, $output = $tasks[$id][$tasks[$id].PSBase.Keys]
-                $output['Success'] = $task.Status -eq [Tasks.TaskStatus]::RanToCompletion
-                $instance.ForEach('Dispose') # Avoid any throws here
-                $tasks.RemoveAt($id)
-                [pscustomobject] $output
-            } while($tasks -and $timer.ElapsedMilliseconds -le $timeout)
-
-            if($timer.ElapsedMilliseconds -gt $timeout) {
-                foreach($t in $tasks) {
-                    $instance, $task, $output = $t[$t.PSBase.Keys]
-                    $output['Success'] = $task.Status -eq [Tasks.TaskStatus]::RanToCompletion
-                    $instance.ForEach('Dispose') # Avoid any throws here
-                    [pscustomobject] $output
-                }
-            }
-        }
     }
     process {
         foreach($t in $Target) {
             foreach($i in $Port) {
-                if($tasks.Count -eq 62) {
-                    Wait-Tasks
-                }
-
                 $tcp = [TCPClient]::new()
                 $tasks.Add([ordered]@{
                     Instance = $tcp
@@ -68,8 +41,25 @@ function Test-TCPConnectionAsync {
         }
     }
     end {
-        Wait-Tasks
+        do {
+            $id = [Task]::WaitAny($tasks.Task, 200)
+            if($id -eq -1) {
+                continue
+            }
+            $instance, $task, $output = $tasks[$id][$tasks[$id].PSBase.Keys]
+            $output['Success'] = $task.Status -eq [TaskStatus]::RanToCompletion
+            $instance.ForEach('Dispose') # Avoid any throws here
+            $tasks.RemoveAt($id)
+            [pscustomobject] $output
+        } while($tasks -and $timer.ElapsedMilliseconds -le $timeout)
+
+        foreach($t in $tasks) {
+            $instance, $task, $output = $t[$t.PSBase.Keys]
+            $output['Success'] = $task.Status -eq [TaskStatus]::RanToCompletion
+            $instance.ForEach('Dispose') # Avoid any throws here
+            [pscustomobject] $output
+        }
     }
 }
 
-'google.com', 'cisco.com', 'amazon.com' | Test-TCPConnectionAsync 80, 443, 8080, 389, 636
+'google.com' + 'cisco.com' + 'amazon.com' | Test-TCPConnectionAsync 80, 443, 8080, 389, 636
