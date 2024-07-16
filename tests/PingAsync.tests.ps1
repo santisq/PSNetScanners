85e676500fd4bef92fa0e1b791ef005f871380c5
Santiago Squarzon <squarzon.santiago@gmail.com>
2024-07-16 14:55:33 -0300

    adding a more pester tests.

--- tests/PingAsync.tests.ps1
+++ tests/PingAsync.tests.ps1
@@ -26,5 +26,41 @@ Describe TestConnectionAsyncCommand {
             $result.DnsResult | Should -BeOfType ([PSNetScanners.DnsSuccess])
             $result.DnsResult.Status | Should -Be ([PSNetScanners.DnsStatus]::Success)
         }
+        It 'DnsFailure' {
+            $result = Test-ConnectionAsync 127.0.0.2
+            $result.DnsResult | Should -BeOfType ([PSNetScanners.DnsFailure])
+            $result.DnsResult.Status | Should -Be ([PSNetScanners.DnsStatus]::Error)
+        }
+    }
+    Context 'TestConnectAsyncCommand' {
+        It 'Parallel Pings' {
+            Measure-Command {
+                1..254 | ForEach-Object { "127.0.0.$_" } | Test-ConnectionAsync
+            } | ForEach-Object TotalMinutes | Should -BeLessThan 2
+        }
+        It 'Stops processing early' {
+            Measure-Command {
+                1..254 | ForEach-Object { "127.0.0.$_" } |
+                    Test-ConnectionAsync |
+                    Select-Object -First 10
+            } | ForEach-Object TotalSeconds | Should -BeLessThan 10
+        }
+    }
+    Context 'Parameters' {
+        It 'TaskTimeoutMilliseconds' {
+            { 1..254 | ForEach-Object { "127.0.0.$_" } |
+                Test-ConnectionAsync -TaskTimeoutMilliseconds 200 -ErrorAction Stop } |
+                Should -Not -Throw
+        }
+        It 'ThrottleLimit' {
+            { 1..254 | ForEach-Object { "127.0.0.$_" } |
+                Test-ConnectionAsync -ThrottleLimit 300 -ErrorAction Stop } |
+                Should -Not -Throw
+        }
+        It 'BufferSize' {
+            { 1..254 | ForEach-Object { "127.0.0.$_" } |
+                Test-ConnectionAsync -BufferSize 1 -ErrorAction Stop } |
+                Should -Not -Throw
+        }
     }
 }
